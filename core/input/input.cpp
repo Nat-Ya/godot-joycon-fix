8982eb5fe68cae9a7854d22a1f164c655de4f9a2
Yaya <ygally@gmail.com>
2026-01-06 19:33:49 +0000

    chore(input): add extra joypad dispatch logs

--- core/input/input.cpp
+++ core/input/input.cpp
@@ -707,10 +707,12 @@ void Input::_parse_input_event_impl(const Ref<InputEvent> &p_event, bool p_is_em
 	Ref<InputEventJoypadButton> jb = p_event;
 
 	if (jb.is_valid()) {
+		print_verbose(vformat("[Input::_parse_input_event_impl] processing joy button device=%d button=%d pressed=%s", jb->get_device(), (int)jb->get_button_index(), jb->is_pressed() ? "true" : "false"));
 		JoyButton c = _combine_device(jb->get_button_index(), jb->get_device());
 
 		if (jb->is_pressed()) {
 			joy_buttons_pressed.insert(c);
+			print_verbose(vformat("[Input::_parse_input_event_impl] inserted device=%d button=%d combined=%d", jb->get_device(), (int)jb->get_button_index(), (int)c));
 		} else {
 			joy_buttons_pressed.erase(c);
 		}
@@ -1243,6 +1245,7 @@ void Input::_button_event(int p_device, JoyButton p_index, bool p_pressed) {
 	ievent->set_device(p_device);
 	ievent->set_button_index(p_index);
 	ievent->set_pressed(p_pressed);
+	print_verbose(vformat("[Input::_button_event] device=%d button=%d pressed=%s", p_device, (int)p_index, p_pressed ? "true" : "false"));
 
 	parse_input_event(ievent);
 }
@@ -1458,6 +1461,11 @@ void Input::parse_mapping(const String &p_mapping) {
 	_THREAD_SAFE_METHOD_;
 	JoyDeviceMapping mapping;
 
+
+	Ref<InputEventJoypadButton> jb_check = p_event;
+	if (jb_check.is_valid()) {
+		print_verbose(vformat("[Input::parse_input_event] button=%d use_accumulated=%s agile=%s", (int)jb_check->get_button_index(), use_accumulated_input ? "true" : "false", agile_input_event_flushing ? "true" : "false"));
+	}
 	Vector<String> entry = p_mapping.split(",");
 	if (entry.size() < 2) {
 		return;
